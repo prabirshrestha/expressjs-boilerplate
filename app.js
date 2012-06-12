b381193770ffd92ab2d49c8109cb5b9f6c860736
prabirshrestha <mail@prabir.me>
2012-06-12 03:12:52 -0400

    routes as object in app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
  */
 
 var   express = require('express')
-    , routes = require('./routes')
+    , routes =  {
+        index: require('./routes')
+    }
     , http = require('http');
 var app = express();
 
@@ -24,7 +26,7 @@ app.configure('development', function(){
     app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', routes.index.index);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
