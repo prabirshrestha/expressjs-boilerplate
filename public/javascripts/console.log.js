caa290c3584f0fbd06d9889467a56cff17ebe7af
prabirshrestha <mail@prabir.me>
2012-06-12 02:38:39 -0400

    console.log.js

---
+++ public/javascripts/console.log.js
@@ -0,0 +1,17 @@
+// make it safe to use console.log always
+
+(function(a) {
+    function b() {}
+    var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn";
+    var d;
+    for (c = c.split(","); !!(d = c.pop());) {
+        a[d] = a[d] || b;
+    }
+})(function() {
+    try {
+        console.log();
+        return window.console;
+    } catch(a) {
+        return (window.console = {});
+    }
+}());
