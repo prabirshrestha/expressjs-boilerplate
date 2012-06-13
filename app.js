20f13ed93b87a026314ea27d11635cb0dc99c005
prabirshrestha <mail@prabir.me>
2012-06-13 19:52:10 -0400

    use connect-assetmanager with snockets

--- app.js
+++ app.js
@@ -3,39 +3,53 @@
  * Module dependencies.
  */
 
-var   express = require('express')
-    , routes =  {
-        index: require('./routes')
-    }
-    , http = require('http')
-    , gzippo = require('gzippo')
-    , connectAssets = require('connect-assets')({ src: __dirname + '/public' });
-var app = express();
+var express = require('express'),
+    http = require('http'),
+    assetManager = require('connect-assetmanager'),
+    assets = require('./assets');
 
-app.configure(function(){
+var app = express(),
+    assetsMiddleware = assetManager(assets.assets);
+
+var routes = {
+    index: require('./routes')
+};
+
+app.configure('development', function () {
+    for(var asset in assets.assets) {
+        if(asset !== 'forEach' && !asset.debug){
+            assets.assets[asset].debug = true;
+        }
+    }
+});
+
+app.configure(function () {
+    // settings
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
+});
+
+app.configure(function() {
+    // middleware
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(function (req, res, next) {
-        res.removeHeader("X-Powered-By");
-        next();
-    });
+    app.use(assetsMiddleware);
+
     app.use(app.router);
 
-    // app.use(express.static(__dirname + '/public'));
-    app.use(gzippo.staticGzip(__dirname + '/public'));
-
-    css.root = "stylesheets";
-    js.root = "javascripts";
-    app.use(connectAssets);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
     app.use(express.errorHandler());
+});
+
+app.locals.use(function (req, res, next) {
+   res.locals.assetsCacheHashes = assetsMiddleware.cacheHashes;
+   next();
 });
 
 app.get('/', routes.index.index);
---
+++ assets.js
@@ -0,0 +1,40 @@
+
+var Snockets = require('snockets'),
+	snockets = new Snockets(),
+	assets = {};
+
+exports.snockets = function(file, path, index, isLast, callback) {
+	return snockets.getConcatenation(path, function(err, js) {
+		if (js) {
+			return callback(js);
+		} else {
+			return callback(err);
+		}
+	});
+};
+
+assets.jquery = {
+	'route': /\/javascripts\/jquery.[a-z0-9]+.*\.js/,
+	'path': './public/javascripts/',
+	'dataType': 'javascript',
+	'files': [
+		'_libs/jquery-1.7.2.js'
+
+	],
+	debug: false
+};
+
+assets.js = {
+	'route': /\/javascripts\/app.[a-z0-9]+.*\.js/,
+	'path': './public/javascripts/',
+	'dataType': 'javascript',
+	'files': [
+		'main.js'
+	],
+	preManipulate:{
+		'^': [exports.snockets]
+	},
+	debug: false
+};
+
+exports.assets = assets;
