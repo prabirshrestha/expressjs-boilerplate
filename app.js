5a946fd3b4a2cbdc33852ec10b03a047849222f9
prabirshrestha <mail@prabir.me>
2012-06-17 18:11:44 -0400

    h5bp middleware

--- app.js
+++ app.js
@@ -26,17 +26,23 @@ app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
 
+    var h5bp = require('./h5bp');
+    app.use(h5bp.ieEdgeChromeFrameHeader());
+    app.use(h5bp.protectDotfiles());
+    app.use(h5bp.blockBackupFiles());
+    app.use(h5bp.removePoweredBy());
+
     app.use(app.router);
 
     app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
 app.get('/', routes.index.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
